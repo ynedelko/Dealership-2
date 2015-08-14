bd80c95a8ba69cbe21be0d50fa96759ce78984a1
Yelena Nedelko <ynedelko@gmail.com>
2015-08-13 21:50:30 -0700

    Add first passing spec and code to return make of the vehicle.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,2 +1,9 @@
 class Vehicle
+  define_method(:initialize) do |make, model, year|
+    @make = make
+  end
+
+  define_method(:make) do
+    @make
+  end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,6 +1,12 @@
 require('rspec')
-require ('scripts')
+require ('vehicle')
 
-describe do |variable|
+describe(Vehicle) do
+end
 
+describe('#make') do
+  it("returns the make of the vehicle") do
+  test_vehicle = Vehicle.new("Toyota", "Yaris", 2013)
+  expect(test_vehicle.make()).to(eq("Toyota"))
+  end
 end
