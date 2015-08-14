20527b2397117967c91abaeea8d5ac0d4adee63f
Yelena Nedelko <ynedelko@gmail.com>
2015-08-14 00:36:26 -0700

    Add all views and routes for Vehicle class.

--- app.rb
+++ app.rb
@@ -6,3 +6,27 @@ require('./lib/vehicle')
 get('/') do
   erb(:index)
 end
+
+get('/vehicles') do
+  @vehicles = Vehicle.all
+  erb(:vehicles)
+end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
+end
+
+get('/vehicle/:id') do
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
+  erb(:vehicle)
+end
