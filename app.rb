0bc35bbe0497e0942754dec653468e991b83ede6
Yelena Nedelko <ynedelko@gmail.com>
2015-08-13 23:08:36 -0700

    Initialize project for web and test route to index page.

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require ('sinatra')
 require ('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/vehicle')
 
 get('/') do
   erb(:index)
--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -2,3 +2,10 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the vehicle path', {:type => :feature}) do
+  it('visits the index page') do
+    visit('/')
+    expect(page).to have_content("Welcome to this Site!")
+  end
+end
