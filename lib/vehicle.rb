4f53e94516c5a72bdd55046258129ea33945a39c
Yelena Nedelko <ynedelko@gmail.com>
2015-08-13 21:55:49 -0700

    Add specs and passing code to return the model and year of the vehicle.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,19 @@
 class Vehicle
   define_method(:initialize) do |make, model, year|
     @make = make
+    @model = model
+    @year = year
   end
 
   define_method(:make) do
     @make
+  end
+
+  define_method(:model) do
+    @model
+  end
+
+  define_method(:year) do
+    @year
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -10,3 +10,17 @@ describe('#make') do
   expect(test_vehicle.make()).to(eq("Toyota"))
   end
 end
+
+describe('#model') do
+  it("returns the model of the vehicle") do
+  test_vehicle = Vehicle.new("Toyota", "Yaris", 2013)
+  expect(test_vehicle.model()).to(eq("Yaris"))
+  end
+end
+
+describe('#year') do
+  it("returns the year of the vehicle") do
+  test_vehicle = Vehicle.new("Toyota", "Yaris", 2013)
+  expect(test_vehicle.year()).to(eq(2013))
+  end
+end
